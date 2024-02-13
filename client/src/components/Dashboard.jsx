113ed487c61ce48c1adef0e1486031eeb15d53f3
Rogério Soares <rgrsoares@yahoo.com.br>
2024-02-13 09:49:58 -0300

    Create the navbar dashboard

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,18 +1,64 @@
-import React from 'react';
+import React from "react";
 
 function Dashboard() {
-    return (
-  
-     <nav className="navbar bg-body-tertiary">
-     <div className="container-fluid">
-       <a className="navbar-brand"><h1>Dashboard</h1></a>
-       <form className="d-flex" role="search">
-         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-         <button className="btn btn-outline-success" type="submit">Search</button>
-       </form>
-     </div>
-   </nav>
-    );
+  return (
+    <nav
+      class="navbar navbar-expand-lg navbar-dark bg-danger"
+      aria-label="Ninth navbar example"
+    >
+      <div class="container-xl">
+        <a class="navbar-brand" href="#">
+          <h1>Dashboard</h1>
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarsExample07XL"
+          aria-controls="navbarsExample07XL"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExample07XL">
+          <span class="navbar-nav me-auto mb-2 mb-lg-0"></span>
+          <div>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Users
+                </a>
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Configs
+                </a>
+                <ul class="dropdown-menu">
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Add new users
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="/logout">
+                      Logout
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Dashboard;
